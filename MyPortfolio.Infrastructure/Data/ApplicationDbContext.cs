126a58c7e33f2eb6a24910e75b9d36a5a984e31a
Mohamed DOUMBOUYA <integredoumbouya@gmail.com>
2025-01-21 14:14:49 +0100

    Ajout des DbSet

--- MyPortfolio.Infrastructure/Data/ApplicationDbContext.cs
+++ MyPortfolio.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,9 +1,25 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using MyPortfolio.Domain.Models;
 
 namespace MyPortfolio.Infrastructure.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Education> Educations { get; set; }
+        public DbSet<Mission> Missions { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Resume> Resumes { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<Technology> Technologies { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+
     }
 }
