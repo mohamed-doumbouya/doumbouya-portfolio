619b6b05ddc3363d41f9b22194665c02aabcc522
Mohamed DOUMBOUYA <integredoumbouya@gmail.com>
2025-01-29 20:06:35 +0100

    Ajout de mappers

---
+++ MyPortfolio.Domain/Mappers/ProjectMapper.cs
@@ -0,0 +1,42 @@
+﻿using MyPortfolio.Domain.DTO;
+using MyPortfolio.Domain.Models;
+using System.Collections.Generic;
+
+namespace MyPortfolio.Domain.Mappers
+{
+    public static class ProjectMapper
+    {
+
+        public static ProjectDto ConvertToProjectDTO(this Project project)
+        {
+            if(project == null)
+            {
+                return null;
+            }
+
+            var projectDto = new ProjectDto();
+
+            projectDto.Name = project.Name;
+            projectDto.ProjectURL = project.ProjectURL;
+            projectDto.Id = project.Id;
+            projectDto.Description = project.Description ?? string.Empty;
+            projectDto.Summary = project.Summary ?? string.Empty;
+            projectDto.Client = project.Client;
+            projectDto.Category = project.Category?.Name ?? string.Empty;
+            projectDto.StartDate = project.StartDate;
+            projectDto.EndDate = project.EndDate;
+            projectDto.ImageUrl = project.ImageUrl;
+            projectDto.Title = project.Title;
+
+            return projectDto;
+        }     
+
+        public static IEnumerable<ProjectDto> ConvertToProjectDtoList(this IEnumerable<Project> projects)
+        {
+            foreach( var project in projects)
+            {
+                yield return ConvertToProjectDTO(project);
+            }
+        }
+    }
+}
---
+++ MyPortfolio.Domain/Mappers/ServiceMapper.cs
@@ -0,0 +1,36 @@
+﻿using MyPortfolio.Domain.DTO;
+using MyPortfolio.Domain.Models;
+using System.Collections.Generic;
+
+namespace MyPortfolio.Domain.Mappers
+{
+    public static class ServiceMapper
+    {
+
+        public static ServiceDto ConvertToServiceDTO(this Service service)
+        {
+            if (service == null)
+            {
+                return null;
+            }
+
+            var serviceDTO = new ServiceDto();
+
+            serviceDTO.Id = service.Id;
+            serviceDTO.Description = service.Description ?? string.Empty;
+            serviceDTO.Summary = service.Summary ?? string.Empty;
+            serviceDTO.Title = service.Title;
+            serviceDTO.ImgUrl = service.ImgUrl ?? string.Empty;
+
+            return serviceDTO;
+        }
+
+        public static IEnumerable<ServiceDto> ConvertToServiceDTOList(this IEnumerable<Service> services)
+        {
+            foreach (var service in services)
+            {
+                yield return ConvertToServiceDTO(service);
+            }
+        }
+    }
+}
