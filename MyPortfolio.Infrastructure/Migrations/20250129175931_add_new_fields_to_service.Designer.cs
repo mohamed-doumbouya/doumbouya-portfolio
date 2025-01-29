3c759977f5fc8f2b469cda3898e51dc37f7d5c9c
Mohamed DOUMBOUYA <integredoumbouya@gmail.com>
2025-01-29 20:07:51 +0100

    Generation de migration pour les nouveaux champs service

--- MyPortfolio.Infrastructure/Migrations/20250129175931_add_new_fields_to_service.Designer.cs
+++ MyPortfolio.Infrastructure/Migrations/20250129175931_add_new_fields_to_service.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyPortfolio.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using MyPortfolio.Infrastructure.Data;
 namespace MyPortfolio.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250129175931_add_new_fields_to_service")]
+    partial class addnewfieldstoservice
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
