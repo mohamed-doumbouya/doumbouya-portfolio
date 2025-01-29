3f2f5d19582607a4158afb229918f8f3b9d681e2
Mohamed DOUMBOUYA <integredoumbouya@gmail.com>
2025-01-29 20:10:12 +0100

    Creation des controllers project et service

---
+++ MyPortfolio/Controllers/ProjectsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.Domain.DTO;
+using MyPortfolio.Domain.Interfaces.Services;
+
+namespace MyPortfolio.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        private readonly IProjectService _projectService;
+
+        public ProjectsController(IProjectService projectService)
+        {
+            _projectService = projectService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Details(int Id)
+        {
+           ProjectDto? project = await _projectService.GetProjectByIdAsync(Id);
+           if(project == null)
+            {
+                return NotFound();
+            }
+
+            return View(project);
+        }
+    }
+}
---
+++ MyPortfolio/Controllers/ServicesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.Domain.DTO;
+using MyPortfolio.Domain.Interfaces.Services;
+
+namespace MyPortfolio.Controllers
+{
+    public class ServicesController : Controller
+    {
+        private readonly IServiceHandler _serviceHandler;
+
+        public ServicesController(IServiceHandler serviceHandler)
+        {
+            _serviceHandler = serviceHandler;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Details(int Id)
+        {
+            ServiceDto? service = await _serviceHandler.GetServiceByAsync(Id);
+
+            if (service == null)
+            {
+                return NotFound();
+            }
+
+            return View(service);
+        }
+    }
+}
