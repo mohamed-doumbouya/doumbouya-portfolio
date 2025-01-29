3d19c0f4cef35695a4868fb44e45fbf50e24c42e
Mohamed DOUMBOUYA <integredoumbouya@gmail.com>
2025-01-29 20:05:10 +0100

    Ajout de nouveaux champs à projectdto et servicedto

--- MyPortfolio.Domain/DTO/ProjectDto.cs
+++ MyPortfolio.Domain/DTO/ProjectDto.cs
@@ -5,13 +5,13 @@ namespace MyPortfolio.Domain.DTO
 {
     public class ProjectDto
     {
-        public int id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string Client { get; set; }
         public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
+        public DateTime? EndDate { get; set; }
         public string? ImageUrl { get; set; }
         public string? ProjectURL { get; set; }
         public string Category { get; set; }
