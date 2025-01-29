e5db18b0d57b964e60fbb3e619eeacff8f7ce4ff
Mohamed DOUMBOUYA <integredoumbouya@gmail.com>
2025-01-29 20:35:16 +0100

    Ajout de accountcontroller

--- MyPortfolio.Domain/Interfaces/Services/IUserService.cs
+++ MyPortfolio.Domain/Interfaces/Services/IUserService.cs
@@ -1,10 +1,12 @@
-﻿using MyPortfolio.Domain.Models.ViewModels;
+﻿using MyPortfolio.Domain.DTO;
+using MyPortfolio.Domain.Models.ViewModels;
 using System.Threading.Tasks;
 
 namespace MyPortfolio.Domain.Interfaces.Services
 {
     public interface IUserService
     {
+        Task<UserDto> GetUserAsync();
         Task<UserInfoViewModel> GetUserInformationsAsync();
     }
 }
---
+++ MyPortfolio/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.Domain.DTO;
+using MyPortfolio.Domain.Interfaces.Services;
+
+namespace MyPortfolio.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public AccountController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Details()
+        {
+            UserDto user = await _userService.GetUserAsync();
+            return View(user);
+        }
+    }
+}
