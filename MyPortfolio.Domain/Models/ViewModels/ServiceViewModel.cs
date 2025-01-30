738fff9d8e8b666df57513624ba92c8e9031d29b
Mohamed DOUMBOUYA <integredoumbouya@gmail.com>
2025-01-30 13:21:01 +0100

    Creation de la page index pour les services

---
+++ MyPortfolio.Domain/Models/ViewModels/ServiceViewModel.cs
@@ -0,0 +1,11 @@
+﻿using MyPortfolio.Domain.DTO;
+using System.Collections.Generic;
+
+namespace MyPortfolio.Domain.Models.ViewModels
+{
+    public class ServiceViewModel
+    {
+        public IEnumerable<ServiceDto> Services { get; set; }
+        public IEnumerable<TestimonialDto> Testimonials { get; set; }
+    }
+}
--- MyPortfolio/Controllers/ServicesController.cs
+++ MyPortfolio/Controllers/ServicesController.cs
@@ -1,21 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MyPortfolio.Domain.DTO;
 using MyPortfolio.Domain.Interfaces.Services;
+using MyPortfolio.Domain.Models.ViewModels;
 
 namespace MyPortfolio.Controllers
 {
     public class ServicesController : Controller
     {
         private readonly IServiceHandler _serviceHandler;
+        private readonly ITestimonialService _testimonialService;
 
-        public ServicesController(IServiceHandler serviceHandler)
+        public ServicesController(IServiceHandler serviceHandler, ITestimonialService testimonialService)
         {
             _serviceHandler = serviceHandler;
+            _testimonialService = testimonialService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var serviceViewModel = new ServiceViewModel();
+
+            serviceViewModel.Services = await _serviceHandler.GetServicesAsync();
+            serviceViewModel.Testimonials = await _testimonialService.GetUserTestimonials();
+            return View(serviceViewModel);
         }
 
         public async Task<IActionResult> Details(int Id)
